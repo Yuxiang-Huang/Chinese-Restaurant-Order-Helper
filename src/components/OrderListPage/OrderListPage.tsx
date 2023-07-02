a1d5eb760283cad8bf920e45386c669cc86c5a60
Yuxiang Huang <yhuang30@stuy.edu>
2023-07-02 01:01:12 -0400

    Add button to show/hide archived orders.

--- src/components/OrderListPage/OrderListPage.tsx
+++ src/components/OrderListPage/OrderListPage.tsx
@@ -1,8 +1,17 @@
-import { Button, List, ListItem, HStack, Box, Text } from "@chakra-ui/react";
+import {
+  Button,
+  List,
+  ListItem,
+  HStack,
+  Box,
+  Text,
+  Switch,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { AiOutlinePlus } from "react-icons/ai";
 import { Order } from "../../App";
 import OrderTopBar from "./OrderTopBar";
+import { useState } from "react";
 
 interface Props {
   orderList: Order[];
@@ -23,13 +32,28 @@ const OrderListPage = ({
   archive,
   unarchive,
 }: Props) => {
+  const [archivedMode, setArchivedMode] = useState(false);
+
   return (
     <>
-      <Link to="/">
-        <Button colorScheme="red" margin={3}>
-          <AiOutlinePlus />
-        </Button>
-      </Link>
+      <HStack justifyContent={"space-between"}>
+        <Link to="/">
+          <Button colorScheme="red" margin={3}>
+            <AiOutlinePlus />
+          </Button>
+        </Link>
+        <HStack>
+          <Switch
+            colorScheme="green"
+            isChecked={archivedMode}
+            onChange={(event) => setArchivedMode(event.target.checked)}
+          />
+          <Text whiteSpace="nowrap" marginRight={3}>
+            Show Archive
+          </Text>
+        </HStack>
+      </HStack>
+
       <List spacing={3} margin={3}>
         {orderList.map((order) => (
           <Box border={"2px"} marginBottom={10} key={order.id}>
@@ -58,38 +82,40 @@ const OrderListPage = ({
         ))}
       </List>
 
-      <List spacing={3} margin={3}>
-        {archivedOrderList.map((order) => (
-          <Box
-            border={"2px"}
-            marginBottom={10}
-            key={order.id}
-            background={"gray"}
-          >
-            <OrderTopBar
-              order={order}
-              pay={pay}
-              updateCustomerDescription={updateCustomerDescription}
-              edit={edit}
-              archive={archive}
-              unarchive={unarchive}
-            />
-            {order.orderItemList.map((orderItem) => (
-              <ListItem key={orderItem.id}>
-                <HStack justifyContent={"space-between"}>
-                  <HStack>
-                    <Box margin={3}>{orderItem.name}</Box>
-                    <Text fontSize="xs">{orderItem.customization}</Text>
+      {archivedMode && (
+        <List spacing={3} margin={3}>
+          {archivedOrderList.map((order) => (
+            <Box
+              border={"2px"}
+              marginBottom={10}
+              key={order.id}
+              background={"gray"}
+            >
+              <OrderTopBar
+                order={order}
+                pay={pay}
+                updateCustomerDescription={updateCustomerDescription}
+                edit={edit}
+                archive={archive}
+                unarchive={unarchive}
+              />
+              {order.orderItemList.map((orderItem) => (
+                <ListItem key={orderItem.id}>
+                  <HStack justifyContent={"space-between"}>
+                    <HStack>
+                      <Box margin={3}>{orderItem.name}</Box>
+                      <Text fontSize="xs">{orderItem.customization}</Text>
+                    </HStack>
+                    <HStack>
+                      <Box margin={3}>{"$" + orderItem.price}</Box>
+                    </HStack>
                   </HStack>
-                  <HStack>
-                    <Box margin={3}>{"$" + orderItem.price}</Box>
-                  </HStack>
-                </HStack>
-              </ListItem>
-            ))}
-          </Box>
-        ))}
-      </List>
+                </ListItem>
+              ))}
+            </Box>
+          ))}
+        </List>
+      )}
     </>
   );
 };
