660b4445865248327b1bbd06b5937281fd5f115d
Yuxiang Huang <yhuang30@stuy.edu>
2023-07-04 21:27:50 -0400

    Hold hot key to show close button to delete it.

--- src/components/AddCustomerPage/Top/HotKeyButton.tsx
+++ src/components/AddCustomerPage/Top/HotKeyButton.tsx
@@ -1,5 +1,5 @@
 import { Button, Box, CloseButton } from "@chakra-ui/react";
-// import { useState } from "react";
+import { useState } from "react";
 
 interface Props {
   hotKey: string;
@@ -8,37 +8,47 @@ interface Props {
 }
 
 const HotKeyButton = ({ hotKey, handleHotKeyClick, removeHotKey }: Props) => {
-  // const [showCloseButton, setShowCloseButton] = useState(false);
-  // const [pressTimer, setPressTimer] = useState(0);
+  // 0 is not show, 1 is show, -1 is preparing for reset
+  const [showCloseButton, setShowCloseButton] = useState(0);
+  const [pressTimer, setPressTimer] = useState(0);
 
-  // const handleButtonPress = () => {
-  //   setPressTimer(setTimeout(() => setShowCloseButton(true), 500));
-  // };
+  const handleButtonPress = () => {
+    setPressTimer(setTimeout(() => setShowCloseButton(1), 500));
+  };
 
-  // const handleButtonRelease = () => {
-  //   clearTimeout(pressTimer);
-  // };
+  const handleButtonRelease = () => {
+    clearTimeout(pressTimer);
+  };
 
   return (
     <Box position="relative" display="inline-block">
       <Button
-        onClick={(event) => handleHotKeyClick(event.currentTarget.innerHTML)}
-        // onMouseDown={handleButtonPress}
-        // onMouseUp={handleButtonRelease}
+        onClick={(event) => {
+          if (showCloseButton === 0) {
+            handleHotKeyClick(event.currentTarget.innerHTML);
+          } else if (showCloseButton === 1) {
+            setShowCloseButton(-1);
+          } else if (showCloseButton === -1) {
+            setShowCloseButton(0);
+          }
+          console.log(showCloseButton);
+        }}
+        onMouseDown={handleButtonPress}
+        onMouseUp={handleButtonRelease}
       >
         {hotKey}
       </Button>
-      {/* {showCloseButton && ( */}
-      <CloseButton
-        position="absolute"
-        top="35%"
-        right={-3}
-        transform="translateY(-100%)"
-        background={"lightgray"}
-        size={"sm"}
-        onClick={() => removeHotKey(hotKey)}
-      ></CloseButton>
-      {/* )} */}
+      {showCloseButton !== 0 && (
+        <CloseButton
+          position="absolute"
+          top="35%"
+          right={-3}
+          transform="translateY(-100%)"
+          background={"lightgray"}
+          size={"sm"}
+          onClick={() => removeHotKey(hotKey)}
+        ></CloseButton>
+      )}
     </Box>
   );
 };
