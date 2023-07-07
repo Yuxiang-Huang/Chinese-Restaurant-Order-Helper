1eef6be34acf7f508708d4fd7b1e97e524fa9a3c
Yuxiang Huang <yhuang30@stuy.edu>
2023-07-07 14:41:11 -0400

    Vegetable options working in rice.

--- src/components/Templates/RiceCustomization.tsx
+++ src/components/Templates/RiceCustomization.tsx
@@ -11,9 +11,8 @@ import {
   Button,
   Input,
   Checkbox,
-  useNumberInput,
   HStack,
-  Flex,
+  Text,
 } from "@chakra-ui/react";
 
 interface Props {
@@ -34,6 +33,45 @@ const RiceCustomization = ({
   // parsing last customization
   let index = 0;
 
+  // vegetables
+  // default onion and beansprout
+  let vegOptions = [true, true, false];
+  if (lastCustomization[index].includes("Veg: ")) {
+    const vegList = lastCustomization[index].substring(5).split(", ");
+    vegOptions = [false, false, false];
+    index++;
+    vegList.map((veg) => {
+      switch (veg) {
+        case "onion":
+          vegOptions[0] = true;
+          break;
+        case "beansprout":
+          vegOptions[1] = true;
+          break;
+        case "scallion":
+          vegOptions[2] = true;
+          break;
+      }
+    });
+  } else if (lastCustomization[index] === "No veg") {
+    vegOptions = [false, false, false];
+    index++;
+  }
+
+  const vegToString = () => {
+    // default so no text
+    if (vegOptions[0] && vegOptions[1] && !vegOptions[2]) return "";
+
+    // no veg case
+    if (!vegOptions[0] && !vegOptions[1] && !vegOptions[2]) return "No veg; ";
+
+    let vegText = "Veg: ";
+    if (vegOptions[0]) vegText += "onion, ";
+    if (vegOptions[1]) vegText += "beansprout, ";
+    if (vegOptions[2]) vegText += "scallion, ";
+    return vegText.substring(0, vegText.length - 2) + "; ";
+  };
+
   // sauce on rice
   let sauceBoolean = false;
   if (lastCustomization[index] == "Sauce on Rice") {
@@ -53,7 +91,7 @@ const RiceCustomization = ({
   const ref = useRef<HTMLInputElement>(null);
 
   const createCustomizationText = () => {
-    let customization = "";
+    let customization = vegToString();
     if (sauceBoolean) customization += "Sauce on Rice; ";
     if (eggBoolean) customization += "Egg; ";
     if (ref.current) customization += ref.current.value;
@@ -72,6 +110,29 @@ const RiceCustomization = ({
           <ModalHeader>Modify Customization</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
+            <Text fontSize={"xl"}>Vegetables</Text>
+            <HStack marginBottom={5} spacing={5}>
+              <Checkbox
+                defaultChecked={vegOptions[0]}
+                onChange={(event) => (vegOptions[0] = event.target.checked)}
+              >
+                Onion
+              </Checkbox>
+              <Checkbox
+                defaultChecked={vegOptions[1]}
+                onChange={(event) => (vegOptions[1] = event.target.checked)}
+              >
+                Beansprout
+              </Checkbox>
+              <Checkbox
+                defaultChecked={vegOptions[2]}
+                onChange={(event) => (vegOptions[2] = event.target.checked)}
+              >
+                Scallion
+              </Checkbox>
+            </HStack>
+
+            <Text fontSize={"xl"}>Options</Text>
             <HStack marginBottom={5} spacing={5}>
               <Checkbox
                 defaultChecked={sauceBoolean}
